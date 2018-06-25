f30ce59e503c3281e088318c7d08d22fb43fecee
juliennuellas <julien.nuellas@gmail.com>
2018-06-25 18:45:49 +0200

    Set up of App structure : still some works to do on static assets

--- purbeurre_platform/purbeurre_platform/urls.py
+++ purbeurre_platform/purbeurre_platform/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.views.generic import RedirectView
+
+from search import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
+    path('search/', include('search.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
