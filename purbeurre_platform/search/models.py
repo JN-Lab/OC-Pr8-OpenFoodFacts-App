2ea8a2f27a841ed9bc7d5dbbc35dd39a92dcf505
juliennuellas <julien.nuellas@gmail.com>
2018-06-28 12:40:56 +0200

    Models integration

--- purbeurre_platform/search/models.py
+++ purbeurre_platform/search/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneFlied(User)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    ref = models.CharField(max_length=100)
+    nutriscore = models.CharField(max_length=1)
+    picture = models.URLField()
+    had_been_registered = models.BooleanField(default=False)
+    last_interaction = models.DateTimeField(default=timezone.now)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    api_id = models.CharField(max_length=200)
+    total_product = models.IntegerField(default=0)
+    enough_good_nutriscore = models.BooleanField(default=False)
+
--- purbeurre_platform/search/views.py
+++ purbeurre_platform/search/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# Home page
 def index(request):
     return render(request, 'index.html')
+
+# Search List
+
+# Product
+
+# Log-in
+
+# Sign-in
+
+# Sign-out
+
+# Personal account
