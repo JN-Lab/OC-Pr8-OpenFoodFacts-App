cd5043067045fd7fa14e1169d5cf7e2e288907bb
juliennuellas <julien.nuellas@gmail.com>
2018-06-29 11:18:30 +0200

    Front: integration of header search form and home search form

--- purbeurre_platform/search/views.py
+++ purbeurre_platform/search/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HeaderSearchForm, HomeSearchForm
 
 # Create your views here.
 
 # Home page
 def index(request):
-    return render(request, 'index.html')
+    header_form = HeaderSearchForm(request.POST or None)
+    home_form = HomeSearchForm(request.POST or None)
+    if header_form.is_valid():
+        pass
+    if home_form.is_valid():
+        pass
+    return render(request, 'index.html', locals())
 
 # Search List
 
