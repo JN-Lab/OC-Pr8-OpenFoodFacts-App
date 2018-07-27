114ff902d6ac8375f57f4fd6f4ac154d364b13e3
juliennuellas <julien.nuellas@gmail.com>
2018-07-27 17:43:08 +0200

    another one test to resolve static problem on production

--- purbeurre_platform/settings.py
+++ purbeurre_platform/settings.py
@@ -150,7 +150,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
-        os.path.join(PROJECT_ROOT, '../static'),
+        os.path.join(PROJECT_ROOT, 'search/static'),
     )
 
     # Simplified static file serving.
