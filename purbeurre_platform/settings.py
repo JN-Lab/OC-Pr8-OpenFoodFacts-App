ecca16f063ed95bc53344e2b3c9c7c754a53953e
juliennuellas <julien.nuellas@gmail.com>
2018-07-28 10:50:56 +0200

    New static config modifications

--- purbeurre_platform/settings.py
+++ purbeurre_platform/settings.py
@@ -150,8 +150,6 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = [
         os.path.join(PROJECT_ROOT, 'static'),
-        os.path.join(BASE_DIR, "static"),
-        os.path.join(BASE_DIR, "search/static"),
     ]
 
     # Simplified static file serving.
