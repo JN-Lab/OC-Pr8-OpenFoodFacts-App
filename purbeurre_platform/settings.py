d1eb6fcfa50f2e4ea7311d3eee897e4681faf7e7
juliennuellas <julien.nuellas@gmail.com>
2018-07-28 12:28:34 +0200

    New static config modifications - base_dir in extra

--- purbeurre_platform/settings.py
+++ purbeurre_platform/settings.py
@@ -144,11 +144,11 @@ INTERNAL_IPS = ['127.0.0.1']
 if os.environ.get('ENV') == 'PRODUCTION':
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
+        os.path.join(PROJECT_ROOT, "static"),
     ]
 
     # Simplified static file serving.
